91587c4c16dfa7fe8d137efcb994f166dbfb6a1d
mosin12 <80879601+mosin12@users.noreply.github.com>
2025-02-14 07:18:52 +0000

    conversion

---
+++ 01basics/coversionOperation.js
@@ -0,0 +1,32 @@
+let score =  "33"
+
+console.log(typeof score)
+
+let valueInNum = Number(score);
+console.log(typeof valueInNum)
+
+
+// let todo =  null
+// let todo1 =  undefined
+// let todo2 =  0
+// console.log(todo2)
+
+
+
+// let isLoggedIn = 1
+//  let booleanIsLOggedIn = Boolean(isLoggedIn)
+
+//  console.log(booleanIsLOggedIn)
+
+ // 1 => true; 0 => false
+ // "" => false
+ // "ramesh" => true
+
+
+ let isNumber =  33
+
+ let convertNum = String(isNumber)
+ console.log(convertNum);
+ console.log(typeof convertNum);
+ 
+ 
