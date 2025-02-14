074fceb43deb1ced9399cf1f599078612cda8b01
mosin12 <80879601+mosin12@users.noreply.github.com>
2025-02-14 06:12:20 +0000

    datatypes

---
+++ 01basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"
+
+let name = 'Mohsin'
+let age = 25
+let isLoggedIn = false
+
+//number 
+// bigint
+// string = ""
+// boolean =  true/false
+// null = standalone value
+// undefined 
+// symbol = unique
+
+// object 
+
+console.log(typeof "Mohsin")  // string
+console.log(typeof null)   // object
+console.log(typeof undefined) // undefined
+
